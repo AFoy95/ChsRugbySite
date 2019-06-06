5ec50e19e6a2d26e7e286b510572bcab9fd194af
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2019-06-06 00:13:16 -0600

    production script

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const path = require("path");
 const app = express();
 // Bodyparser middleware
 app.use(
@@ -27,5 +28,13 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+//serve static assets if in production
+if(process.env.NODE_ENV === "prodution"){
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get('*',(req,res)=>{
+      res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+  });
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
